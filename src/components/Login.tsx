edc478c8bbda705b589fa9c41310980be667e696
oxbowtarsier <oliver.fox@durham.ac.uk>
2024-04-18 22:42:49 +0100

    set different screens for login and signup

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,6 +9,7 @@ const Login = ({storedUsers}: LoginProps) => {
     const [emailInput, setEmailInput] = useState("");
     const [passwordInput, setPasswordInput] = useState("");
     const [hidden, setHidden] = useState("password")
+    const [signUp, setSignUp] = useState(false)
 
     const handleEmailChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         setEmailInput(event.target.value);
@@ -29,7 +30,9 @@ const Login = ({storedUsers}: LoginProps) => {
 
             <div className="row text-white mt-4">
             <div className="col-5">
-            <h1 className="text-center display-4">Log in</h1>
+            {signUp === false ? 
+            <h1 className="text-center display-4">Log in</h1> : 
+            <h1 className="text-center display-4">Sign up</h1>}
 
             <div className="my-3 mx-5">
                 <label htmlFor="emailInput" className="form-label">Email address</label>
@@ -53,14 +56,24 @@ const Login = ({storedUsers}: LoginProps) => {
                 />
             </div>
             </div>
-
+            {! signUp &&
             <div className="col me-5">
                 <h1 className="display-3"><b><s>No Account,</s> No Problem:</b></h1>
                 <br/>
                 <h5>Sign up today to have your say in the GOAT debate. Its as easy as a two-yard tap-in!</h5>
                 <br/>
-                <h5><a href="#">Click here</a> to create an account!</h5>
+                <h5> Simply <a href="#Log in" onClick={() => {setSignUp(true)}}>click here</a> to create an account!</h5>
             </div>
+            }
+            {signUp &&
+            <div className="col me-5">
+                <h1 className="display-3"><b>Already have an Account?</b></h1>
+                <br/>
+                <h5>Get straight to the point. Vote on who you think is the better baller right now!</h5>
+                <br/>
+                <h5><a href="#Log in" onClick={() => {setSignUp(false)}}>click here</a> to log into your account!</h5>
+            </div>
+            }
             </div>
 
             <div className="mb-3 mx-5 form-check">
